9ba2f8c7a37739743f20402a4c2ca4c1e82e5e36
Jonathan Swartz <swartz@pobox.com>
2001-01-02 13:48:16 +0000

    fix preload documentation (bug #203); add a few validation tests for preload values
    git-svn-id: https://svn.urth.org/svn/Mason/branches/mason-0-8-branch@746 fa5c2c93-f9fe-0310-b630-c7cdf19d91a9

--- lib/HTML/Mason/Interp.pm
+++ lib/HTML/Mason/Interp.pm
@@ -172,7 +172,9 @@ sub _initialize
     # Preloads
     #
     if ($self->preloads) {
+	die "list reference expected for preloads parameter" unless ref($self->preloads) eq 'ARRAY';
 	foreach my $pattern (@{$self->preloads}) {
+	    die "preloads pattern must be an absolute path" unless is_absolute_path($pattern);
 	    my @paths = $self->resolver->glob_path($pattern,$self);
 	    foreach (@paths) { $self->load($_) }
 	}
