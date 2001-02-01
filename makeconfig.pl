23ae78ff3d6c54e0061bd467e732f7a308c0d93e
Dave Rolsky <autarch@urth.org>
2001-02-01 06:11:25 +0000

    incorporate Apache test setup/cleanup the code in makeconfig.pl should make sure that if PERL5LIB is set that it will get used with the Apache tests.  I tested on my box and it worked.
    git-svn-id: https://svn.urth.org/svn/Mason/branches/mason-1-0-branch@809 fa5c2c93-f9fe-0310-b630-c7cdf19d91a9

--- makeconfig.pl
+++ makeconfig.pl
@@ -1,3 +1,5 @@
+use strict;
+
 my $dbmWarnMsg = <<EOF;
 ==> WARNING: the DBM file format chosen for your system, %s, is
 inadequate for data caching due to size limitations. If you intend to
@@ -72,20 +74,24 @@ sub have_pkg
 
 sub chk_version
 {
- my($pkg,$wanted,$msg) = @_;
+    my($pkg,$wanted,$msg) = @_;
 
- local($|) = 1;
- print "Checking for $pkg...";
+    local($|) = 1;
+    print "Checking for $pkg...";
 
- eval { my $p; ($p = $pkg . ".pm") =~ s#::#/#g; require $p; };
+    eval { my $p; ($p = $pkg . ".pm") =~ s#::#/#g; require $p; };
 
- my $vstr = ${"${pkg}::VERSION"} ? "found v" . ${"${pkg}::VERSION"}
-				 : "not found";
- my $vnum = ${"${pkg}::VERSION"} || 0;
+    my $vstr;
+    my $vnum;
+    {
+	no strict 'refs';
+	$vstr = ${"${pkg}::VERSION"} ? "found v" . ${"${pkg}::VERSION"}	: "not found";
+	$vnum = ${"${pkg}::VERSION"} || 0;
+    }
 
- print $vnum >= $wanted ? "ok\n" : " " . $vstr . "\n";
+    print $vnum >= $wanted ? "ok\n" : " " . $vstr . "\n";
 
- $vnum >= $wanted;
+    $vnum >= $wanted;
 }
 
 sub make_config
@@ -99,7 +105,8 @@ sub make_config
     my %c = %HTML::Mason::Config;
     
     $c{default_cache_tie_class} ||= 'MLDBM';
-    
+
+    my $val;
     if (!defined($c{mldbm_use_db})) {
 	if (defined($MLDBM::UseDB) && $MLDBM::UseDB !~ /^SDBM|ODBM|NDBM/) {
 	    $val = $MLDBM::UseDB;
@@ -147,7 +154,7 @@ sub make_config
 
     if (!defined($c{use_time_hires})) {
 	print "\n";
-	my $h = chk_version(Time::HiRes => '1.19');
+	my $h = chk_version('Time::HiRes' => '1.19');
 	print $hiresWarnMsg if !$h;
 	$c{use_time_hires} = $h;
     }
@@ -162,5 +169,238 @@ sub make_config
     print join("\n",grep(/=>/,split("\n",$conf)))."\n\n";
     print $successMsg,"-"x20,"\n";
 }
-    
+
+use lib 'lib', 't/lib';
+
+use Cwd;
+use File::Path;
+use File::Basename;
+
+use vars qw(%APACHE);
+
+sub setup_mod_perl_tests
+{
+    # Skip if no mod_perl
+    eval { require mod_perl; };
+    return if $@;
+
+    require Apache::test;
+
+    eval { require Apache::Request; };
+    $APACHE{has_apache_request} = $@ ? 0 : 1;
+
+    cleanup_files();
+
+    write_apache_conf();
+    setup_handler('CGI');
+    write_test_comps();
+
+
+    setup_handler('mod_perl') if $APACHE{has_apache_request};
+}
+
+sub cleanup_files
+{
+    foreach ( qw( httpd httpd.conf mason_handler_CGI.pl mason_handler_mod_perl.pl ) )
+    {
+	my $file = "t/$_";
+	if ( -e $file )
+	{
+	    unlink $file
+		or die "Can't unlink '$file': $!";
+	}
+    }
+
+    foreach ( qw( comps data ) )
+    {
+	my $dir = "t/$_";
+	if ( -d $dir )
+	{
+	    rmtree( $dir, $ENV{MASON_DEBUG} );
+	}
+    }
+}
+
+sub write_apache_conf
+{
+    my %p = Apache::test->get_test_params();
+    while (my ($k, $v) = each %p)
+    {
+	$APACHE{$k} = $v;
+    }
+
+    my $cwd = cwd();
+    my $conf_file = $APACHE{conf_file} ? "$cwd/$APACHE{conf_file}" : "$cwd/t/httpd.conf";
+    $APACHE{apache_dir} = dirname($conf_file);
+    $APACHE{apache_dir} =~ s,/$,,;
+
+    $APACHE{comp_root} = "$APACHE{apache_dir}/comps";
+    $APACHE{data_dir} = "$APACHE{apache_dir}/data";
+
+    mkdir $APACHE{comp_root}, 0755
+	or die "Can't make dir '$APACHE{comp_root}': $!";
+    mkdir $APACHE{data_dir}, 0755
+	or die "Can't make dir '$APACHE{comp_root}': $!";
+
+    my $include .= <<"EOF";
+
+<IfDefine CGI>
+  PerlRequire $APACHE{apache_dir}/mason_handler_CGI.pl
+
+  <Location /mason>
+    SetHandler perl-script
+    PerlHandler HTML::Mason
+  </Location>
+
+  <Location /mason_stream>
+    SetHandler perl-script
+    PerlHandler HTML::Mason
+  </Location>
+</IfDefine>
+EOF
+
+    if ($APACHE{has_apache_request})
+    {
+	$include .= <<"EOF"
+
+<IfDefine mod_perl>
+  PerlRequire $APACHE{apache_dir}/mason_handler_mod_perl.pl
+
+  <Location /mason>
+    SetHandler perl-script
+    PerlHandler HTML::Mason
+  </Location>
+
+  <Location /mason_stream>
+    SetHandler perl-script
+    PerlHandler HTML::Mason
+  </Location>
+</IfDefine>
+EOF
+    }
+
+    local $^W;
+    Apache::test->write_httpd_conf
+	    ( %APACHE,
+	      include => $include
+	    );
+}
+
+sub setup_handler
+{
+    my $args_method = shift;
+
+    my $handler = "mason_handler_$args_method.pl";
+    my $handler_file = "$APACHE{apache_dir}/$handler";
+    open F, ">$handler_file"
+	or die "Can't write to '$handler_file': $!";
+
+    my $libs = '';
+    if ($ENV{PERL5LIB})
+    {
+	$libs = 'use lib qw( ';
+	$libs .= join ' ', (split /:|;/, $ENV{PERL5LIB});
+	$libs .= ' );';
+    }
+
+    print F <<"EOF";
+package HTML::Mason;
+
+$libs
+
+use HTML::Mason::ApacheHandler ( args_method => '$args_method' );
+use HTML::Mason;
+
+my \$parser = HTML::Mason::Parser->new;
+my \$interp = HTML::Mason::Interp->new( parser => \$parser,
+					comp_root => '$APACHE{comp_root}',
+					data_dir => '$APACHE{data_dir}' );
+
+my \$stream_ah = HTML::Mason::ApacheHandler->new( interp => \$interp,
+                                                 output_mode => 'stream' );
+my \$batch_ah = HTML::Mason::ApacheHandler->new( interp => \$interp,
+                                                output_mode => 'batch' );
+
+sub handler
+{
+    my \$r = shift;
+    \$r->header_out('X-Mason-Test' => 'Initial value');
+
+    my \$ah = \$r->uri =~ /mason_stream/ ? \$stream_ah : \$batch_ah;
+
+    # strip off stuff just used to figure out what handler to use.
+    my \$filename = \$r->filename;
+    \$filename =~ s,/mason(?:_stream)?\$,,;
+
+    \$filename .= \$r->path_info;
+    \$filename =~ s,//+,/,g;
+
+    \$r->filename(\$filename);
+
+    my \$status = \$ah->handle_request(\$r);
+    \$r->print( "Status code: \$status\\n" );
+}
+EOF
+    close F;
+}
+
+sub write_test_comps
+{
+    write_comp( 'basic', <<'EOF',
+Basic test.
+2 + 2 = <% 2 + 2 %>.
+uri = <% $r->uri =~ /basic$/ ? '/basic' : $r->uri %>.
+method = <% $r->method %>.
+
+
+EOF
+	      );
+
+    write_comp( 'headers', <<'EOF',
+
+
+% $r->header_out('X-Mason-Test' => 'New value 2');
+Blah blah
+blah
+% $r->header_out('X-Mason-Test' => 'New value 3');
+<%init>
+$r->header_out('X-Mason-Test' => 'New value 1');
+$m->abort if $blank;
+</%init>
+<%args>
+$blank=>0
+</%args>
+EOF
+	      );
+
+    write_comp( 'cgi_object', <<'EOF',
+<% $m->cgi_object->isa('CGI') ? 'CGI' : 'NO CGI' %>
+EOF
+	      );
+
+    if ($APACHE{has_apache_request})
+    {
+	write_comp( 'apache_request', <<'EOF',
+<% ref $r %>
+EOF
+		  );
+    }
+}
+
+sub write_comp
+{
+    my $name = shift;
+    my $comp = shift;
+
+    my $file = "$APACHE{comp_root}/$name";
+    open F, ">$file"
+	or die "Can't write to '$file': $!";
+
+    print F $comp;
+
+    close F;
+}
+
+
+
 1;
