33771c0febf98b8dfcd62ac5b13722b7c86da467
Jonathan Swartz <swartz@pobox.com>
2001-08-17 05:16:11 +0000

    fix find sub in make_dirs
    git-svn-id: https://svn.urth.org/svn/Mason/branches/mason-1-0-branch@1136 fa5c2c93-f9fe-0310-b630-c7cdf19d91a9

--- lib/HTML/Mason/Parser.pm
+++ lib/HTML/Mason/Parser.pm
@@ -1251,7 +1251,7 @@ sub make_dirs
     }
 
     my $compilesub = sub {
-	my ($srcfile) = @_;
+	my $srcfile = $File::Find::name;
 	return if (!-f $srcfile);
 	return if defined($predicate) && !($predicate->($srcfile));
 	my $compPath = substr($srcfile,length($source_dir));
